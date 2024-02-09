81d8a82623e6ee7d3053fb575dbedc0d330fd4f7
DrSloughKeg <bpye1995@gmail.com>
2024-02-09 15:25:46 -0500

    all inputs and temp converter

---
+++ Day02AllInputs/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day02AllInputs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TbxName.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Name must not be empty", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            //also ugly fix
+            string age;
+            if (Rbn1835.IsChecked == true)
+            {
+                age = "18-35";
+            } else if (Rbn18Under.IsChecked == true)
+            {
+                age = "under 18";
+            } else if (Rbn35Up.IsChecked == true) { age = "over 35"; }
+            else
+            {
+                MessageBox.Show("How did you unselect that?", "Input error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            //very ugly fix
+            string pets = "";
+            if (CbxCat.IsChecked == true || CbxDog.IsChecked == true || CbxOther.IsChecked == true) {
+                bool first = true;
+                if (CbxCat.IsChecked == true)
+                {
+                    pets += "Cat";
+
+                    first = false;
+                }
+                if (CbxDog.IsChecked == true)
+                {
+                    if (!first)
+                    {
+                        pets += ",";
+                    }
+                    pets += "Dog";
+                    first = false;
+                }
+                if (CbxOther.IsChecked == true)
+                {
+                    if (!first)
+                    {
+                        pets += ",";
+                    }
+                    pets += "Other";
+                }
+            }
+            int caseStr = Cmb.SelectedIndex;
+            string continent;
+            switch (caseStr){
+                case 0:
+                    continent = "North America";
+                    break;
+                case 1:
+                    continent = "South America";
+                    break;
+                case 2:
+                    continent = "Europe";
+                    break;
+                case 3:
+                    continent = "Africa";
+                    break;
+                case 4:
+                    continent = "Asia";
+                    break;
+                case 5:
+                    continent = "Australia";
+                    break;
+                default:
+                    continent = "";
+                    break;
+            } 
+
+            string temp = TbxTemp.Text;
+
+            string info = name + ";" + age + ";";
+            if (pets != "")
+            {
+                info += pets + ";";
+            }
+            info += continent + ";" + temp;
+
+            string[] infoAry = { info };
+
+            const string filePath = @"..\..\info.txt";
+            try
+            {
+
+                File.WriteAllLines(filePath, infoAry);
+            }
+            catch (SystemException ex) { Console.WriteLine("Error writing to file: " + ex.Message); }
+
+        }
+
+    }
+}
---
+++ Day02TempConvert/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day02TempConvert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string inputStr = TbxInput.Text;
+
+            double input;
+            if (!double.TryParse(inputStr, out input))
+            {
+                MessageBox.Show("Try using a real number");
+                return;
+            }
+
+            bool inCels = InCels.IsChecked == true;
+            bool inFahr = InFahr.IsChecked == true;
+            bool inKelv = InKelv.IsChecked == true;
+            bool outCels = OutCels.IsChecked == true;
+            bool outFahr = OutFahr.IsChecked == true;
+            bool outKelv = OutKelv.IsChecked == true;
+
+
+
+            double output;
+            if ((inCels && outCels) || (inFahr && outFahr) || (inKelv && outKelv)) {
+                TbxOut.Text = inputStr;
+            }
+            else if (inCels && outFahr)
+            {
+                output = (input * 9 / 5) + 32;
+                TbxOut.Text = output.ToString();
+            } else if (inCels && outKelv)
+            {
+                output = input + 273.15;
+                TbxOut.Text = output.ToString();
+            } else if (inFahr && outCels)
+            {
+                output = (input - 32) * 5 / 9;
+                TbxOut.Text = output.ToString();
+            } else if (inFahr && outKelv)
+            {
+                output = (input - 32) * 5 / 9 + 273.15;
+                TbxOut.Text = output.ToString();
+            } else if (inKelv && outCels)
+            {
+                output = input - 273.15;
+                TbxOut.Text = output.ToString();
+            } else if (inKelv && outFahr)
+            {
+                output = (input - 273.15) * 9 / 5 + 32;
+                TbxOut.Text = output.ToString();
+            }
+
+        }
+    }
+}
