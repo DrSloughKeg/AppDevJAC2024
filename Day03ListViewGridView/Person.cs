1afdfb87b75d763e08bb2f935a2909e2614dad2a
DrSloughKeg <bpye1995@gmail.com>
2024-02-12 16:35:10 -0500

    listview & List gridView

---
+++ Day03ListViewGridView/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03ListViewGridView
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public static bool IsNameValid(string name, out string error)
+        {
+            if (name.Length < 2 || name.Length > 50 || name == null)
+            {
+                error = "Name is not valid.";
+                return false;
+            } else {
+                error = "";
+                return true;
+            }
+        }
+        public static bool IsAgeValid(int age, out string error)
+        {
+            if (age < 0 ||  age > 100 )
+            {
+                error = "Age is not valid";
+                return false;
+            }
+            else
+            {
+                error = "";
+                return true;
+            }
+        }
+    }
+}
