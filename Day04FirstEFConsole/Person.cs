c00ec17323d0cd48e3ea52dcec3d40183b33975b
DrSloughKeg <bpye1995@gmail.com>
2024-02-13 18:54:41 -0500

    day04 :)

---
+++ Day04FirstEFConsole/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04FirstEFConsole
+{
+    public class Person
+    {
+        //[key]
+        public int Id { get; set; }
+
+        [Index] // wont help with LIKE '%jerry%' === contains("Jerry") will help with LIKE "Jerry%"
+        [Required] // not null
+        [StringLength(50)] //nvarchar 50
+        public string Name { get; set; }
+
+        [Required]
+        [Index]
+        public int Age { get; set; }
+
+        //TODO find out how to set up a unique index
+
+        /*
+         * [NotMapped] //in memory only (e.g. computer), not in database
+         * public string Comment {get;set;}
+         * 
+         * [EnumDataType(typeof(GenderEnum))]
+         * public GenderEnum Gender { get; set; }
+         * public enum GenderEnum { Male = 1, Female = 2, Other = 3, NA = 4}
+         */
+    }
+}
---
+++ Day04FirstEFConsole/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04FirstEFConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SocietyDbContext db = new SocietyDbContext();
+                Random random = new Random();
+                Person p1 = new Person { Name = "Jerry " + random.Next(1000), Age = random.Next(100) };
+                db.People.Add(p1); // insert operation is scheduled but not executed yet
+                db.SaveChanges(); // synchronize objects in memory with database
+                Console.WriteLine("Record added");
+
+                //UPDATE; equivalent of of update fetch then modify and save changes
+                //firstOrDefault will either return Person or null
+                Person p2 = (from p in db.People where p.Id == 2 select p).FirstOrDefault<Person>();
+                if (p2 != null )
+                {
+                    p2.Name = "Alabma " + (random.Next(10000) + 10000); //entity framework is watching and notices the modification
+                    db.SaveChanges(); // update the datebase to synchronize entities in memory with the database
+                    Console.WriteLine("Record Updated.");
+                }
+                else
+                {
+                    Console.WriteLine("Record not found");
+                }
+
+                //delete - fetch schedule for deletion then save changes
+                Person p3 = (from p in db.People where p.Id == 1 select p).FirstOrDefault<Person>();
+                if (p3 != null )
+                { // found the record
+                    db.People.Remove(p3);
+                    db.SaveChanges();
+                    Console.WriteLine("Record Deleted");
+                } else
+                {
+                    Console.WriteLine("Record not found");
+                }
+
+                //fetch all records
+                List<Person> peopleList = (from p in db.People select p).ToList<Person>();
+                foreach (Person p in peopleList)
+                {
+                    Console.WriteLine($"{p.Id}: {p.Name} is {p.Age} years old.");
+                }
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine("Database operation failed: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
